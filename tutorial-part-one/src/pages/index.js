c7509015e35c5986a8fa3cb56362e95e3b16eb4f
zac-seth <zac.seth@gmail.com>
2018-01-25 16:05:40 +0100

    Complete first build

--- tutorial-part-one/src/pages/index.js
+++ tutorial-part-one/src/pages/index.js
@@ -1,3 +1,16 @@
-import React from "react"
+import React from 'react'
+import Link from 'gatsby-link'
 
-export default () => <div>Hello world!</div>
+export default () => 
+  <div style={{ color: 'tomato' }}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <br />
+    <div>
+      <Link to="/page-2/">Link</Link>
+    </div>
+    <div>
+      <Link to="/counter/">Counter</Link>
+    </div>
+  </div>
